e192492a2cad33a37beecdf46711498fcfcd7d52
Isaac <izaroot327@gmail.com>
2021-04-19 16:20:01 -0400

    Some CSS and HTML

---
+++ index.js
@@ -0,0 +1,31 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<div class="card">
+    <img src="https://image.api.playstation.com/cdn/UP0006/CUSA10866_00/3uebKTl6KgC0chEhBOs3GVaOLapwPgQ5.png">
+    <p>Burnout Paradise Remastered</p>
+    <p>Genre: Racing</p>
+    <div id="platforms">
+        <img src="https://cdn.icon-icons.com/icons2/688/PNG/512/windows-software-operating-system_icon-icons.com_61426.png">
+    </div>
+</div>
